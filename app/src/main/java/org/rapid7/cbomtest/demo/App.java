97daec6a32058479b1a7821ce3bff8d8757ae2a7
dshafranskiy-r7 <dmitriy_shafranskiy@rapid7.com>
2024-05-29 15:07:34 +0200

    test

--- app/src/main/java/org/rapid7/cbomtest/demo/App.java
+++ app/src/main/java/org/rapid7/cbomtest/demo/App.java
@@ -2,7 +2,7 @@ package com.rapid7.cbomtest.demo;
 
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "Hello World22!";
     }
 
     public static void main(String[] args) {
